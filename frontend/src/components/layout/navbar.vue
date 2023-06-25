3c2297ef44361302b527417e84e79eb9dd084e58
Phuc lucas <dangvanphuc7122001@gmail.com>
2023-06-25 23:04:30 +0700

    update code avatar employee

--- frontend/src/components/layout/navbar.vue
+++ frontend/src/components/layout/navbar.vue
@@ -242,7 +242,7 @@ export default {
 
     // In hoa ký tự đầu
     const capitalizeFirstLetter = (string) => {
-      const words = string.split("");
+      const words = string.split(" ");
       const capitalizedWords = words.map((word) => {
         return word.charAt(0).toUpperCase() + word.slice(1);
       });
